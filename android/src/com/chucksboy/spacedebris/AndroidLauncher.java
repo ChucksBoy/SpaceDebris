cd1b9cb27b9d82fd730a490232ba3646f68319ab
Kyle Chamberlin <KyleChamberlin@project20million.org>
2016-06-10 14:34:51 -0400

    android

---
+++ android/src/com/chucksboy/spacedebris/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.chucksboy.spacedebris;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.chucksboy.spacedebris.SpaceDebrisMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceDebrisMain(), config);
+	}
+}
